438d17191a8b21deef23b578688365ebf692b72f
denys tretiak <dtretiak@drbsystems.com>
2021-02-03 22:50:22 +0200

    rename planet component

---
+++ src/app/components/planet-cards/planet-cards.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Planet } from 'src/app/interfaces/planet.interface';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+    selector: 'app-planet-cards',
+    templateUrl: './planet-cards.component.html',
+    styleUrls: ['./planet-cards.component.scss']
+})
+export class PlanetСardsComponent implements OnInit, OnDestroy {
+    planets: Planet[];
+    page = 1;
+    error: Error;
+
+    private destroyed$ = new Subject<void>();
+
+    constructor(
+        private apiService: ApiService,
+        private router: Router
+    ) {}
+
+    ngOnInit(): void {
+        this.apiService.getPlanets()
+            .pipe(takeUntil(this.destroyed$))
+            .subscribe((planets) => this.planets = planets);
+    }
+
+    ngOnDestroy(): void {
+        this.destroyed$.next();
+        this.destroyed$.complete();
+    }
+
+    loadMore(): void {
+        this.apiService.getPlanets(++this.page).pipe(takeUntil(this.destroyed$)).subscribe(
+            (planets) => this.planets = this.planets.concat(planets),
+            (error) => this.error = error
+        );
+    }
+
+    navigateToPlanet(url: string): void{
+        const numberPattern = /\d+/g;
+
+        url = url.match( numberPattern )[0];
+        this.router.navigate(['planet',  url]);
+    }
+}
